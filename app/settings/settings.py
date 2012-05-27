61a27b2a357fbef9ae45be8ca7461d3d59837ff6
Chris Hager <chris@metachris.org>
2012-05-28 00:36:42 +0200

    fab script

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -41,47 +41,6 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-# Change in accordance with STATIC_ROOT to serve with outside webserver too
-MEDIA_ROOT = '/opt/photosite/media/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8080/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/opt/photosite/static/'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "/Users/chris/Projects/private/django/photosite/app/static/",
-    )
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    )
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -102,14 +61,6 @@ if DEBUG:
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
 ROOT_URLCONF = 'app.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates"
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "/Users/chris/Projects/private/django/photosite/app/templates",
-    "/Users/chris/Projects/private/django/photosite/env/lib/python2.7/site-packages/treebeard/templates",
-    )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
--- fabfile.py
+++ fabfile.py
@@ -3,16 +3,46 @@ from fabric.api import *
 env.use_ssh_config = True
 env.hosts = ["hetzner1"]
 
-REMOTE_DIR = "/var/www/chrishager.at/django-photoblog"
+LOCAL_TMP_DIR = "/tmp/photoblog-build"
+
+LOCAL_STATIC_PARENT_DIR = "/opt/photosite"
+
+REMOTE_DIR = "/var/www/chrishager.at"
+
+
+def pack():
+    # Clean up temp dir
+    local("rm -rf %s" % LOCAL_TMP_DIR)
+    local("mkdir -p %s" % LOCAL_TMP_DIR)
+
+    # Pack
+    local("tar -czhf %s/app.tar.gz --exclude='static' --exclude='*.pyc' app/" % LOCAL_TMP_DIR)
+    with lcd(LOCAL_STATIC_PARENT_DIR):
+        local("tar -czf %s/media.tar.gz --exclude='.DS_Store' media" % (LOCAL_TMP_DIR))
+        local("tar -czf %s/static.tar.gz --exclude='.DS_Store' --exclude='twitter-bootstrap' static" % (LOCAL_TMP_DIR))
 
 
 def deploy():
-    local("tar -czf app.tar.gz app/")
-    run("rm -rf %s" % REMOTE_DIR)
-    put('app.tar.gz', "%s/" % REMOTE_DIR)
+    pack()
+
+    # Upload
+    put('%s/app.tar.gz' % LOCAL_TMP_DIR, "%s/" % REMOTE_DIR)
+    put('%s/media.tar.gz'% LOCAL_TMP_DIR, "%s/" % REMOTE_DIR)
+    put('%s/static.tar.gz' % LOCAL_TMP_DIR, "%s/" % REMOTE_DIR)
     put('dependencies.txt', "%s/" % REMOTE_DIR)
+
+    # Remove old version
+    run("rm -rf %s/app" % (REMOTE_DIR))
+    run("rm -rf %s/static" % (REMOTE_DIR))
+
+    # Unpack and remove
     with cd(REMOTE_DIR):
         run("tar zxf app.tar.gz")
+        run("tar zxf media.tar.gz")
+        run("tar zxf static.tar.gz")
+        run("rm -f app.tar.gz media.tar.gz static.tar.gz")
+
+    # Restart
     restart()
 
 
