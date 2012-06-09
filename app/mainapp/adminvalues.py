a36e9448eea2caeff2fa8a216efd8ecbf10f6360
Chris Hager <chris@metachris.org>
2012-06-09 21:09:24 +0200

    AdminValues doc update

--- app/mainapp/adminvalues.py
+++ app/mainapp/adminvalues.py
@@ -1,47 +1,49 @@
 """
-AdminValues are settings which can have the default (eg. in settings.py)
-but can be overwritten by creating a models.AdminValue object with the
-specified key.
+Settings which can have a local default (eg. from settings.py, ...) but can be
+overwritten by creating a models.AdminValue object with the specified key (eg.
+in the Django admin interface).
 
-Source code must simply reference adminvalues.AdminValues.ADMINVALUE_VAR instead
+Source code must simply reference adminvalues.YOUR_VALUE instead
 of a hard-coded setting.
 """
 import mainapp.models as models
 from django.conf import settings
 
 
-# Add any fields here that you wish.
-# ADMINVALUE_VAR = (models.AdminValue.key, default_obj=None)
-PHOTOFLOW_LAYOUTS = ("photoflow_layouts", [0])
-PHOTOFLOW_LAYOUTS_TEST = ("photoflow_layouts_test", [0])
-
-PHOTOGRID_ITEMS_INITIAL = ("photogrid_items_initial", settings.PHOTOGRID_ITEMS_INITIAL)
-PHOTOGRID_ITEMS_PERPAGE = ("photogrid_items_perpage", settings.PHOTOGRID_ITEMS_PERPAGE)
-
-PHOTOFLOW_BLOCKS_INITIAL = ("photoflow_blocks_initial", settings.PHOTOFLOW_BLOCKS_INITIAL)
-PHOTOFLOW_BLOCKS_PERPAGE = ("photoflow_blocks_perpage", settings.PHOTOFLOW_BLOCKS_PERPAGE)
+class AdminValue(object):
+    """ Simple AdminValue Container """
+    def __init__(self, db_key, default=None):
+        self.db_key = db_key
+        self.default = default
 
 
-def get(key):
+# AdminValue declarations. Add any fields that you want here.
+PHOTOFLOW_LAYOUTS = AdminValue("photoflow_layouts", [0])
+PHOTOFLOW_LAYOUTS_TEST = AdminValue("photoflow_layouts_test", [0])
+
+PHOTOGRID_ITEMS_INITIAL = AdminValue("photogrid_items_initial", settings.PHOTOGRID_ITEMS_INITIAL)
+PHOTOGRID_ITEMS_PERPAGE = AdminValue("photogrid_items_perpage", settings.PHOTOGRID_ITEMS_PERPAGE)
+
+PHOTOFLOW_BLOCKS_INITIAL = AdminValue("photoflow_blocks_initial", settings.PHOTOFLOW_BLOCKS_INITIAL)
+PHOTOFLOW_BLOCKS_PERPAGE = AdminValue("photoflow_blocks_perpage", settings.PHOTOFLOW_BLOCKS_PERPAGE)
+
+
+def get(av):
     """
     Returns the value of the models.AdminValue object if it exists, else
     returns the hard-coded default value.
 
-    key needs to be a tuple of (<db_key>, <default>.
-
-    Use like this: adminvalues.get(adminvalues.PHOTOFLOW_LAYOUTS)
+    Example usage: adminvalues.get(adminvalues.PHOTOFLOW_LAYOUTS)
     """
-    db_key, default = key
-
     try:
-        ret = models.AdminValue.objects.get(key=db_key).val
+        ret = models.AdminValue.objects.get(key=av.db_key).val
     except models.AdminValue.DoesNotExist:
-        ret = default
+        ret = av.default
 
-#    print "AdminValues: '%s' = '%s'" % (key, ret)
+    #print "AdminValues: '%s' = '%s'" % (av.db_key, ret)
     return ret
 
 
-def get_int(key):
+def get_int(av):
     """Return an int (force cast)"""
-    return int(get(key))
+    return int(get(av))
